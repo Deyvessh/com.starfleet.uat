6fd6cf78154a57f51dcc1cfc6a4201279836b27c
deyvessh_kumar <deyvesshkumar@gmail.com>
2025-09-25 11:51:18 +0530

    added code directory #216

---
+++ src/main/java/com/starfleet/blackrock/com/starfleet/uat/App.java
@@ -0,0 +1,13 @@
+package com.starfleet.blackrock.com.starfleet.uat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
